3ef370875268cf66302df25d5f654b82cd74a895
michfuer <mike.fuerstnau@commerceguys.com>
2012-08-24 16:14:33 -0400

    added js files to the module directory

---
+++ js/panel.js
@@ -0,0 +1,21 @@
+google.maps.event.addDomListener(window, 'load', function() {
+  var map = new google.maps.Map(document.getElementById('map-canvas'), {
+    //Default center on North America.
+    center: new google.maps.LatLng(40, -100),
+    zoom: 4,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
+  var panelDiv = document.getElementById('panel');
+
+  var data = new MedicareDataSource;
+
+  var view = new storeLocator.View(map, data, {
+    geolocation: false,
+    features: data.getFeatures()
+  });
+
+  new storeLocator.Panel(panelDiv, {
+    view: view
+  });
+});
